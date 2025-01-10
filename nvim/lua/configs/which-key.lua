12abc15c95bb496ee69c458741d44c53945b22c9
Henrique M. Dimer <hdimer@tutamail.com>
2025-01-09 22:14:48 -0300

    feat(which-key): add keybindings for rename and find implementation
    Added new keybindings:
    - `<leader>cr` for renaming code
    - `<leader>ci` for finding implementation

--- nvim/lua/configs/which-key.lua
+++ nvim/lua/configs/which-key.lua
@@ -9,6 +9,8 @@ wk.add({
 	{ "<leader>c",  desc = "+Code Actions" },
 	{ "<leader>cf", ":lua vim.lsp.buf.format({ async = true})<CR>", desc = "Format Code" },
 	{ "<leader>ca", ":lua vim.lsp.buf.code_action()<CR>",           desc = "Code Actions" },
+	{ "<leader>cr", ":lua vim.lsp.buf.rename()<CR>",                desc = "Rename code" },
+	{ "<leader>ci", ":lua vim.lsp.buf.implementation()<CR>",        desc = "Find Implementation" },
 	{ "<leader>e",  ":Neotree focus<CR>",                           desc = "Focus File Tree" },
 	{ "<leader>t",  ":ToggleTerm<CR>",                              desc = "Open Terminal" },
 	{ "<leader>g",  ":LazyGit<CR>",                                 desc = "LazyGit" }
