2d27d3d8e1f94e2f209930c408fbff36a537cef2
Henrique M. Dimer <hdimer@tutamail.com>
2025-03-22 14:31:45 -0300

    feat: brand new minimal neovim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,43 +1,10 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
-
-vim.wo.relativenumber = true
 vim.g.mapleader = " "
-vim.o.shiftwidth = 4
-vim.o.softtabstop = 4
-vim.o.tabstop = 4
-vim.o.ignorecase = true
-vim.o.smartcase = true
 
-require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = false },
-});
+require("configs.lazy")
+require("configs.keymaps")
 
-vim.api.nvim_create_autocmd("UIEnter", {
-    callback = function()
-		vim.cmd[[syntax on]]
-		vim.o.termguicolors = true
-		vim.cmd[[colorscheme carbonfox]]
-    end,
-})
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "number"
+vim.opt.ignorecase = true
+vim.diagnostic.config({ virtual_text = false })
--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,119 +1,25 @@
 return {
-	'AbdelrahmanDwedar/awesome-nvim-colorschemes',
 	{
-		'lewis6991/gitsigns.nvim',
-		event = 'VeryLazy',
+		'ms-jpq/chadtree',
+		branch = 'chad',
+		build = 'python4 -m chadtree deps',
+		lazy = true,
+		event = "VeryLazy"
+	},
+	{
+		's1n7ax/nvim-terminal',
+		event = "VeryLazy",
+		lazy = true,
 		config = function()
-			require('configs.gitsigns')
-		end
-	},
-	{
-		'folke/todo-comments.nvim',
-		event = 'VeryLazy',
-		config = function()
-			require('todo-comments').setup {}
-		end
-	},
-	{
-		'j-hui/fidget.nvim',
-		event = 'VeryLazy',
-		config = function()
-			require('fidget').setup {}
-		end
-	},
-	{
-		"kawre/leetcode.nvim",
-		event = 'VeryLazy',
-		dependencies = {
-			"ibhagwan/fzf-lua",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-		},
-		config = function()
-			require('leetcode').setup {}
-		end
-	},
-	{
-		'ibhagwan/fzf-lua',
-		event = 'VeryLazy',
-	},
-	-- {
-	-- 	'wakatime/vim-wakatime',
-	-- 	event = 'VeryLazy',
-	-- },
-	{
-		'kdheepak/lazygit.nvim',
-		event = 'VeryLazy',
-	},
-	{
-		'rachartier/tiny-inline-diagnostic.nvim',
-		event = 'VeryLazy',
-		priority = 1000, -- needs to be loaded in first
-		config = function()
-			require('tiny-inline-diagnostic').setup {}
-			vim.diagnostic.config({ virtual_text = false })
-		end
-	},
-	{
-		'akinsho/toggleterm.nvim',
-		event = 'VeryLazy',
-		config = function()
-			require('toggleterm').setup()
-		end
-	},
-	{
-		'nvim-neo-tree/neo-tree.nvim',
-		branch = 'v3.x',
-		event = 'VeryLazy',
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			'nvim-tree/nvim-web-devicons',
-			'MunifTanjim/nui.nvim',
-		},
-		config = function()
-			require('configs.neotree')
-		end
-	},
-	{
-		'folke/which-key.nvim',
-		lazy = false,
-		config = function()
-			require('configs.which-key')
-		end
-	},
-	{
-		'olimorris/codecompanion.nvim',
-		event = 'VeryLazy',
-		config = function()
-			require("codecompanion").setup({
-				adapters = {
-					openai = function()
-						return require("codecompanion.adapters").extend("openai", {
-							env = {
-								api_key =
-								"",
-							},
-							schema = {
-								model = {
-									default = "gpt-4o"
-								}
-							}
-						})
-					end,
-				},
-				strategies = {
-					chat = {
-						adapter = "openai",
-					},
-					inline = {
-						adapter = "openai",
-					},
-				},
-			})
+			vim.o.hidden = true
+			require('nvim-terminal').setup()
 		end,
-		dependencies = {
-			'nvim-lua/plenary.nvim',
-			'nvim-treesitter/nvim-treesitter',
-		}
 	},
+	{
+		"ibhagwan/fzf-lua",
+		event = "VeryLazy",
+		lazy = true,
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {}
+	}
 }
