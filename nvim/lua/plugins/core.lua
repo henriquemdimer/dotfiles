d5591a9fb6ccd5f12945735cd1d52b6f74e3f3b9
Henrique M. Dimer <hdimer@tutamail.com>
2025-03-22 19:18:45 -0300

    feat: add gitsigns plugin

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,5 +1,13 @@
 return {
 	{
+		'lewis6991/gitsigns.nvim',
+		event = "VeryLazy",
+		lazy = true,
+		config = function ()
+			require('configs.gitsigns')
+		end
+	},
+	{
 		'ms-jpq/chadtree',
 		branch = 'chad',
 		build = 'python4 -m chadtree deps',
